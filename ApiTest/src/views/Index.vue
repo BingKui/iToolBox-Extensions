ec88348fbbd8d6d2b684c9a3718e44d03a62794d
康兵奎 <BingKui@users.noreply.github.com>
2020-08-13 23:11:18 +0800

    add apitest

---
+++ ApiTest/src/views/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        API 测试
+        <button @click="addDbItem">添加项</button>
+    </div>
+</template>
+
+<script>
+const iTools = window.iTools;
+export default {
+    name: 'Index',
+    methods: {
+        addDbItem() {
+            iTools.addItem('test-db', {
+                title: '新数据',
+                name: 'hehe',
+            }).then(flag => {
+                if (flag) {
+                    alert('添加成功！');
+                } else {
+                    alert('添加失败');
+                }
+            });
+        },
+    },
+};
+</script>
+
+<style lang="less">
+
+</style>
