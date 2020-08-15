343fd60e2b0fc5e06a563a925dea82f55ca8afbb
BingKui <178974662@qq.com>
2020-08-15 15:03:25 +0800

    add API Test

---
+++ API/src/views/Index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="v-index-page">
+        <Divider orientation="center">示例项目</Divider>
+        <div class="desc">本项目为示例项目，主要包含基本的框架，打包封装。可以零基础直接进行插件的开发。内置包含 "ant-design-vue" 组件库，方便快速构建界面。</div>
+        <Divider orientation="left">AntdV基础组件展示</Divider>
+        <div class="components-list">
+            <Button>按钮</Button>
+            <Icon type="smile" theme="twoTone" style="fontSize: 25px;" />
+        </div>
+        <Divider orientation="left">API 基础</Divider>
+        <Button @click="apiVerAction">获取api版本</Button>
+        <Button @click="apiNotice">系统提醒</Button>
+        <div v-if="apiVersion" class="api-version">Api Version: v{{apiVersion}}</div>
+        <Divider orientation="left">数据库操作</Divider>
+        <DBTest />
+    </div>
+</template>
+
+<script>
+import { Button, Divider, Icon } from 'ant-design-vue';
+import DBTest from './components/DBTest';
+const iTools = window.iTools;
+export default {
+    name: 'Index',
+    components: {
+        Button,
+        Divider,
+        Icon,
+        DBTest,
+    },
+    data() {
+        return {
+            apiVersion: '',
+        };
+    },
+    methods: {
+        apiVerAction() {
+            this.apiVersion = iTools.version;
+        },
+        async apiNotice() {
+            await iTools.sysNotice('API 测试', '这个是提示的内容~~~~~');
+        },
+    },
+};
+</script>
+
+<style lang="less">
+.v-index-page {
+    .p(@gap-md);
+    .desc {
+        .font-size();
+        color: @text-color;
+        text-indent: @font-size * 2;
+    }
+    .components-list {
+        .flex();
+        align-items: center;
+    }
+}
+</style>
---
+++ API/src/views/components/DBTest.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="v-db-test">
+        <!-- 添加数据 -->
+        <Card class="margin-btoom" hoverable title="添加数据">
+            <Input class="margin-btoom" v-model="name" placeholder="输入内容进行保存" />
+            <Button class="margin-btoom" @click="addItemData">保存</Button>
+            <div class="add-result" v-if="addResult">保存结果：{{addResult}}</div>
+        </Card>
+        <!-- 数据列表 -->
+        <Card class="margin-btoom" title="数据列表">
+            <Button class="margin-btoom" @click="getAllDataList">获取本地数据列表</Button>
+            <Button class="margin-btoom" @click="getAllDataCount">统计总数</Button>
+            <List :grid="{ gutter: 15, column: 1 }" :data-source="dbList">
+                <ListItem slot="renderItem" slot-scope="item, index" :key="index">
+                    {{item.name}}
+                    <Button slot="actions" type="danger" @click="() => deleteItemData(item)">删除</Button>
+                    <Button slot="actions" type="primary" @click="() => getItemData(item)">获取详情</Button>
+                    <Button slot="actions" @click="() => updateItemData(item)">更新信息</Button>
+                </ListItem>
+            </List>
+        </Card>
+        <Card v-if="itemDetail" class="margin-btoom" title="单条详情">
+            {{itemDetail}}
+        </Card>
+    </div>
+</template>
+
+<script>
+import { Button, Divider, Icon, Input, message, Card, List } from 'ant-design-vue';
+const DB = window.iTools.DB;
+const DB_NAME = 'api-db';
+export default {
+    name: 'DBTest',
+    components: {
+        Input,
+        Button,
+        Card,
+        List,
+        ListItem: List.Item,
+    },
+    data() {
+        return {
+            name: '',
+            addResult: '',
+            dbList: [],
+            itemDetail: null,
+        };
+    },
+    async mounted() {
+        await this.getAllDataList();
+    },
+    methods: {
+        async addItemData() {
+            const res = await DB.addItem(DB_NAME, {
+                name: this.name,
+            });
+            if (res) {
+                this.addResult = JSON.stringify(res);
+                await this.getAllDataList();
+            }
+        },
+        async getItemData(item) {
+            const res = await DB.getItemById(DB_NAME, item._id);
+            if (res.code === 200) {
+                this.itemDetail = JSON.stringify(res.data);
+            }
+        },
+        async updateItemData(item) {
+            const _item = {
+                name: `${item.name}-update`
+            };
+            const res = await DB.updateItemById(DB_NAME, item._id, _item);
+            if (res.code === 200) {
+                message.success('更新成功');
+                await this.getAllDataList();
+            }
+        },
+        async deleteItemData(item) {
+            console.log('API => 执行删除', item);
+            const res = await DB.deleteItemById(DB_NAME, item._id);
+            if (res.code === 200) {
+                message.success('删除成功');
+                await this.getAllDataList();
+            }
+        },
+        async getAllDataList() {
+            const res = await DB.getAllItems(DB_NAME);
+            console.log('API => 获取所有数据', res);
+            if (res) {
+                this.dbList = res.data;
+            }
+        },
+        async getAllDataCount() {
+            const res = await DB.getCount(DB_NAME);
+            if (res) {
+                message.success(`获取统计信息成功：总共${res.data}条数据！`);
+            }
+        },
+    },
+};
+</script>
+
+<style lang="less">
+.v-db-test {
+    .margin-btoom {
+        .m-b(@gap-md);
+    }
+}
+</style>
